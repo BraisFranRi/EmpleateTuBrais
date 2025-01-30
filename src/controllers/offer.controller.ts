31f26f22a78e951cd66b651c9b1275288a90d697
BraisFranRi <braisfranri@gmail.com>
2025-01-30 09:54:03 +0100

    offer controller fixed

---
+++ src/controllers/offer.controller.ts
@@ -0,0 +1,32 @@
+import {Response, Request, NextFunction} from 'express'
+
+export class OfferController{
+
+    static async getAll(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async getById(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async save(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async delete(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async update(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async rate(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+
+    static async getRate(req:Request, res:Response, next:NextFunction){
+        console.log('Get all offers')
+    }
+}
--- src/routes/offer.routes.ts
+++ src/routes/offer.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
-
+import {OfferController} from "@controllers/offer.controller";
 const router = Router()
 
 // localhost:3000/api/offerts/
 // localhost:3000/api/offerts/?title=react&category=DAM
-router.get('/', OffertController.getAll) // Listar ofertas
+router.get('/', OfferController.getAll) // Listar ofertas
 // POST localhost:3000/api/offerts/ {body}
-router.get('/:id', OffertController.getById)
-router.post('/', OffertController.save) // Añadir oferta (Admins)
+router.get('/:id', OfferController.getById)
+router.post('/', OfferController.save) // Añadir oferta (Admins)
 // GET localhost:3000/api/offerts/XXXX (Es un parámetro que se manda en la ruta tras el último /)
-router.delete('/:id', OffertController.delete) // Borrar una oferta (Admins)
+router.delete('/:id', OfferController.delete) // Borrar una oferta (Admins)
 // PUT/PATCH localhost:3000/api/offert/XXXX {body} -> Los datos nuevos y el id de la oferta a cambiar
 // PUT -> Actualiza todo || PATCH -> Modifica solo una cosa
-router.put('/:id', OffertController.update) // Modificar una oferta 
+router.put('/:id', OfferController.update) // Modificar una oferta 
 
 //POST con id de la oferta valor en el {body}
-router.post('/:id/rate/', OffertController.rate) // Calificamos una oferta x  
+router.post('/:id/rate/', OfferController.rate) // Calificamos una oferta x  
 router.get('/:id/rate/', OfferController.getRate)// Vemos que calificación (total) tiene una oferta?
 
 export default router
