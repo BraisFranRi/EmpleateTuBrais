6dce084d94ce51c2a5081ac5e4dba1aa63e2c3be
BraisFranRi <braisfranri@gmail.com>
2025-01-30 09:36:48 +0100

    Commit en main antes de empezar a usar render

--- src/routes/offer.routes.ts
+++ src/routes/offer.routes.ts
@@ -6,11 +6,16 @@ const router = Router()
 // localhost:3000/api/offerts/?title=react&category=DAM
 router.get('/', OffertController.getAll) // Listar ofertas
 // POST localhost:3000/api/offerts/ {body}
+router.get('/:id', OffertController.getById)
 router.post('/', OffertController.save) // Añadir oferta (Admins)
 // GET localhost:3000/api/offerts/XXXX (Es un parámetro que se manda en la ruta tras el último /)
 router.delete('/:id', OffertController.delete) // Borrar una oferta (Admins)
 // PUT/PATCH localhost:3000/api/offert/XXXX {body} -> Los datos nuevos y el id de la oferta a cambiar
 // PUT -> Actualiza todo || PATCH -> Modifica solo una cosa
 router.put('/:id', OffertController.update) // Modificar una oferta 
+
+//POST con id de la oferta valor en el {body}
+router.post('/:id/rate/', OffertController.rate) // Calificamos una oferta x  
+router.get('/:id/rate/', OfferController.getRate)// Vemos que calificación (total) tiene una oferta?
 
 export default router
