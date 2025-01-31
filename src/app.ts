3bbcfd3b3f2efc9fbabd93cb9f7a8d01ea4ca20a
BraisFranRi <braisfranri@gmail.com>
2025-01-31 09:33:06 +0100

    Prisma y npm i regenerados

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(cookieParser())
 // To do limitar cors 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
